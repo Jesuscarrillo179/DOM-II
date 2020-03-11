99b39a831fe55ea9b3acf3ef578a848173dd5788
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-03-11 15:31:44 -0500

    added everything else

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ const headImg = document.querySelector(".intro img");
 const textColor = document.querySelector("h1");
 const headerText = document.querySelectorAll("h2");
 const infoText = document.querySelectorAll("p");
+const navItems = document.querySelectorAll("#nav-link");
+const tourImages = document.querySelectorAll(".img-content img");
+const largeimg = document.querySelector(".content-destination img");
 
-console.log(headerText)
 
 //event listeners
 
@@ -14,11 +16,13 @@ disappearButton.addEventListener("dblclick", goodbye);
 document.addEventListener("mousemove", dcrsize);
 document.addEventListener("keypress", incrsize);
 textColor.addEventListener("mouseover", cblue);
+largeimg.addEventListener("scroll", () => {largeimg.style.width = "100%"});
 
 //functions
 
 function goodbye(){
     disappearButton.style.display = "none";
+    disappearButton.stopPropagation();
 };
 
 function dcrsize(){
@@ -42,4 +46,15 @@ for (let i = 0; i < headerText.length; i++){
 for (let i = 0; i < infoText.length; i++){
     infoText[i].addEventListener("mouseleave", () => {infoText[i].style.color = "orange";})
     ;};
-    
+
+for (let i = 0; i < navItems.length; i++){
+    navItems[i].addEventListener("click", () => {navItems[i].preventDefault();})
+   ;};
+
+for (let i = 0; i < tourImages.length; i++){
+    tourImages[i].addEventListener("mousedown", () => {tourImages[i].style.display = "none";})
+    ;};
+
+for (let i = 0; i < tourImages.length; i++){
+    tourImages[i].addEventListener("keydown", () => {tourImages[i].style.display = "inline";})
+    ;};
